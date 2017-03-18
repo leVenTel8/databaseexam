e4938aca2fa26fc1033c70863ae202327b981c97
Vitaly Frolov <leventel8@yandex.ru>
2017-03-18 21:51:46 +0400

    подключил JDBC поменял версию компилятора javac, теперь можно передавать аргументы в try catch

--- src/main/java/ru/leventel8/database/Main.java
+++ src/main/java/ru/leventel8/database/Main.java
@@ -2,10 +2,7 @@ package ru.leventel8.database;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost:3306/myfirstdb";
@@ -13,24 +10,18 @@ public class Main {
     private static final String PASSWORD = "root";
 
     public static void main(String[] args) {
-        Connection connection;
-
         try {
             Driver driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
-
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if(!connection.isClosed()){
-                System.out.println("Соединение с БД установленно!");
-            }
-
-            connection.close();
-
-            if(connection.isClosed()){
-                System.out.println("Соединение с БД закрыто!");
-            }
         } catch (SQLException e){
             System.err.println("Неудалось загрузить класс драйвера!");
+        }
+
+        try(Connection connection = DriverManager.getConnection(URL, USERNAME,PASSWORD); Statement statement = connection.createStatement()){
+            ResultSet res = statement.executeQuery("select * from users;");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
